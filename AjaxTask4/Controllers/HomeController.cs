681c5695aa7f9a9189729ff981db6cd5336b77bd
yash prajapati <yash prajapati@DESKTOP-3TVAUS8>
2019-09-15 09:53:42 +0530

    Add project files.

---
+++ AjaxTask4/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using AjaxTask4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxTask4.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseAjaxEntities db = new DatabaseAjaxEntities();
+        public ActionResult Index()
+        {
+            List<CountryMaster> CountryList = db.CountryMasters.ToList();
+            ViewBag.CountryList = new SelectList(CountryList, "CountryId", "CountryName");
+            return View();
+        }
+
+        public JsonResult GetStateList(int CountryId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<StateMaster> StateList = db.StateMasters.Where(x => x.RefCountryId == CountryId).ToList();
+            return Json(StateList, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetCityList(int StateId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<CityMaster> CityList = db.CityMasters.Where(x => x.RefStateId == StateId).ToList();
+            return Json(CityList, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ AjaxTask4/Models/CountryStateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxTask4.Models
+{
+    public class CountryStateViewModel
+    {
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+        public int CityId { get; set; }
+    }
+}
